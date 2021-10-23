7e02e61b21b1aa159aa7a6d1a3a1346438310ebe
Tom <enclavio@gmail.com>
2021-10-23 23:30:11 +0200

    Removed unnecessary lines

--- main.py
+++ main.py
@@ -17,27 +17,3 @@ event_loop.create_task((leds_program.start(0, 20)))
 
 event_loop.run_forever()
 
-
-# async def main():
-#     uasyncio.create_task((pump_program.start(3, 1)))
-#     uasyncio.create_task((leds_program.test()))
-#     while True:
-#         print("test")
-#         await uasyncio.sleep_ms(1000)
-
-# uasyncio.run(main())
-
-# ntptime.settime()
-
-# _, _, _, utc_time_hour, _, _, _, _ = localtime()
-# dk_time_hour = utc_time_hour + 2
-
-# _, _, _, _, utc_time_minutes, _, _, _ = localtime()
-# dk_time_minutes = utc_time_minutes
-
-# pump_power = Pin(19, Pin.OUT)
-# led_lights = Pin(18, Pin.OUT)
-
-# is_pumping = False
-# lights_are_on = False
-
