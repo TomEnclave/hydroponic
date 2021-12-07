ba0e96f1b6d252d05b3e5af308332ad344144f25
TomEnclave <hoinbeck@gmail.com>
2021-12-07 23:48:46 +0100

    Added spectrum factors

--- light_sensor.py
+++ light_sensor.py
@@ -21,9 +21,34 @@ class Ppfd(timer.Automation):
         return self.i2c.readfrom(self.bh1750_address, 2)
     
     async def measure_ppfd(self, sensor_data, spectrum = "red-blue"):
-        spectrum = 20 if spectrum is "red-blue" else 10
+        if spectrum is "red": #650nm
+            other_spectrum = 13.0122950819672
+        elif spectrum is "blue": #450nm
+            other_spectrum = 8.65410497981157
+        elif spectrum is "red-blue-white":
+            other_spectrum = 38.9265536723164
+        else: # day light
+            other_spectrum = 43.4782608695652
+        """ 
+        Spectrum factors:
+            43.4782608695652 for Daylight
+            28.9136150234742 for Halogen Lamp 3000K
+            58.0260303687636 for High CRI LED 6500K
+            56.1085972850679 for High CRI LED 4000K
+            52.5557011795544 for High CRI LED 3000K
+            74.5889387144992 for Low CRI LED 6500K
+            62.3559498956159 for Low CRI LED 3500K
+            77.079295154185 for HPS 2000K
+            55.092987804878 for CMH 3000K
+            74.1128205128205 for Fluorescent Lamp 5000K
+            13.0122950819672 for Monochromatic Red LED 650 nm
+            8.65410497981157 for Monochromatic Blue LED 450 nm
+            11.2700369913687 for Red + Blue LED 450+650 nm
+            38.9265536723164 for Red + Blue + White LED 450+650nm+3500K
+        """
+        spectrum = 11.2700369913687 if spectrum is "red-blue" else other_spectrum
         lux = (sensor_data[0]<<8 | sensor_data[1]) / 1.2
-        ppfd = lux * 20 / spectrum
+        ppfd = (lux / spectrum * 100) / 100 #umol/s/m2
         return ppfd
 
     async def start_log(self, update_interval=60):
