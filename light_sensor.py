df9b31037d5d1f0ddc45bbcb80ff2b6dd1e50bbc
TomEnclave <hoinbeck@gmail.com>
2021-12-18 22:39:06 +0100

    changed timings, i2c reads are stable now

--- light_sensor.py
+++ light_sensor.py
@@ -4,15 +4,16 @@ import cloud
 
 class Ppfd(timer.Automation):
 
-    def __init__(self, scl_pin, sda_pin, bh1750_address = 0x23):
+    def __init__(self, scl_pin = 22, sda_pin = 21, bh1750_address = 0x23):
         import machine
-        self.i2c = machine.I2C(machine.Pin(scl_pin),machine.Pin(sda_pin))
+        super().__init__()
+        self.i2c = machine.SoftI2C(scl = machine.Pin(scl_pin),sda = machine.Pin(sda_pin))
         self.bh1750_address = bh1750_address
         self.i2c.writeto(self.bh1750_address, bytes([0x01])) #Starts the sensor
     
     async def get_hi_res_value(self):
         self.i2c.writeto(self.bh1750_address, bytes([0x20])) #0x21 can be used too, but the result of measure_ppfd function needs to be multiplied by 2
-        await uasyncio.sleep_ms(180)
+        await uasyncio.sleep_ms(120 * 2) #120ms is the documented response time, but needs atleast 2 times more here, otherwise drops "OSError: [Errno 110] ETIMEDOUT" error
         return self.i2c.readfrom(self.bh1750_address, 2)
     
     async def get_low_res_value(self):
@@ -20,7 +21,7 @@ class Ppfd(timer.Automation):
         await uasyncio.sleep_ms(24)
         return self.i2c.readfrom(self.bh1750_address, 2)
     
-    async def measure_ppfd(self, sensor_data, spectrum = "red-blue"):
+    def measure_ppfd(self, sensor_data, spectrum = "red-blue"):
         if spectrum is "red": #650nm
             other_spectrum = 13.0122950819672
         elif spectrum is "blue": #450nm
@@ -49,6 +50,10 @@ class Ppfd(timer.Automation):
         spectrum = 11.2700369913687 if spectrum is "red-blue" else other_spectrum
         lux = (sensor_data[0]<<8 | sensor_data[1]) / 1.2
         ppfd = (lux / spectrum * 100) / 100 #umol/s/m2
+        print("lux:")
+        print(lux)
+        print("ppfd:")
+        print(ppfd)
         return ppfd
 
     async def start_log(self, update_interval=60):
@@ -56,6 +61,7 @@ class Ppfd(timer.Automation):
         log = cloud.Iot("ppfd")
 
         while True:
+            hi_res = await self.get_hi_res_value()
             self.update_time()
-            log.send({"hour": self.current_hour, "minute": self.current_minute, "ppfd": self.measure_ppfd(self.get_hi_res_value())})
+            log.send({"hour": self.current_hour, "minute": self.current_minute, "ppfd": self.measure_ppfd(hi_res)})
             await uasyncio.sleep(update_interval)
